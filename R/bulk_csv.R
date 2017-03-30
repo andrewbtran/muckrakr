f6fd0f37710db63bedce299ca82221216dc4f9d7
andrewbtran <abtran@gmail.com>
2017-03-30 12:14:54 -0400

    cleaned up bulk_csv

--- R/bulk_csv.R
+++ R/bulk_csv.R
@@ -4,18 +4,59 @@
 #' @param folder The folder in relation to your working directory where the csv files exist. Default folder is the current working directory.
 #' @param export File name to export csv file as, if wanted.
 #' @keywords data appending
-#' @import readr dplyr
 #' @export
 #' @examples
 #' bulk_csv(folder="data", export="combined_data.csv")
 
+# This is the read.csv method using lapply
+
+#bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv"){
+#  require(dplyr)
+#
+#  if (folder=="DEFAULTBULKCSV2017" | folder=="") {
+#    folder <- getwd()
+#  } else {
+#    folder <- paste0(getwd(), "/", folder)
+#  }
+#  files = list.files(folder, pattern="*.csv")
+#  tbl <- lapply(paste0(folder, "/", files), read.csv) %>% bind_rows()
+#  return(tbl)
+#  if (export!="filenamedefaultbulkcsv2017.csv" & export!="") {
+#    write_csv(tbl, export)
+#  }
+#}
+
+# this is the readr version but it loops and has a progress bar
+
 bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv"){
+  require(readr)
+  require(dplyr)
+
   if (folder=="DEFAULTBULKCSV2017" | folder=="") {
     folder <- getwd()
+  } else {
+    folder <- paste0(getwd(), "/", folder)
   }
+
   files = list.files(folder, pattern="*.csv")
-  tbl = lapply(files, read_csv) %>% bind_rows()
-  if (export!="filenamedefaultbulkcsv2017.csv" & export!="") {
-    write_csv(tbl, export)
+
+  pb <- txtProgressBar(min = 0, max = length(files), style = 3)
+
+  for (i in 1:length(files)) {
+    file_x <- read_csv(paste0(folder, "/", files[i]))
+    if (i ==1) {
+      all_files <- file_x
+    } else {
+      all_files <- rbind(all_files, file_x)
+    }
+    setTxtProgressBar(pb, i)
+
   }
+
+  if (export!="filenamedefaultbulkcsv2017.csv" & export!="") {
+    write_csv(all_files, export)
+  }
+  return(all_files)
+
 }
+
