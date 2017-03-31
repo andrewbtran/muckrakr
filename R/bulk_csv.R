0a23a628dccaf3895d6e149589f9498e08c00b20
andrewbtran <abtran@gmail.com>
2017-03-30 23:49:14 -0400

    added bulk documentation

--- R/bulk_csv.R
+++ R/bulk_csv.R
@@ -1,6 +1,6 @@
 #' Creating a single data frame from a folder of similarly structure CSV spreadsheets
 #'
-#' Like Voltron-ing your data
+#' Like Voltron-ing your data. This package is based on readr, so passing on variables from read_csv will work in bulk_csv. These variables will apply to the import of each CSV sheet.
 #' @param folder The folder in relation to your working directory where the csv files exist. Default folder is the current working directory.
 #' @param export File name to export csv file as, if wanted.
 #' @keywords data appending
@@ -10,7 +10,7 @@
 
 # This is the read.csv method using lapply
 
-#bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv"){
+#bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv", ){
 #  require(dplyr)
 #
 #  if (folder=="DEFAULTBULKCSV2017" | folder=="") {
@@ -28,9 +28,24 @@
 
 # this is the readr version but it loops and has a progress bar
 
-bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv"){
+bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv", col_names = TRUE, col_types = NULL,
+                     locale = default_locale(), na = c("", "NA"), quoted_na = TRUE,
+                     comment = "", trim_ws = TRUE, skip = 0, n_max = Inf,
+                     guess_max = min(1000, n_max), progress = interactive()){
   require(readr)
   require(dplyr)
+
+  col_names_f <- col_names
+  col_types_f <- col_types
+  locale_f <- locale
+  na_f <- na
+  quoted_na_f <- quoted_na
+  comment_f <- comment
+  trim_ws_f <- trim_ws
+  skip_f <- skip
+  n_max_f <- n_max
+  guess_max_f <- guess_max
+  progress_f <- progress
 
   if (folder=="DEFAULTBULKCSV2017" | folder=="") {
     folder <- getwd()
@@ -43,7 +58,10 @@ bulk_csv <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv
   pb <- txtProgressBar(min = 0, max = length(files), style = 3)
 
   for (i in 1:length(files)) {
-    file_x <- read_csv(paste0(folder, "/", files[i]))
+    file_x <- read_csv(paste0(folder, "/", files[i]), , col_names = col_names_f, col_types = col_types_f,
+                       locale = local_f, na = na_f, quoted_na = quoted_na_f,
+                       comment = comment_f, trim_ws = trim_ws_f, skip = skip_f, n_max = n_max_f,
+                       guess_max = guess_max_f, progress = progress_f)
     if (i ==1) {
       all_files <- file_x
     } else {
--- R/bulk_excel.R
+++ R/bulk_excel.R
@@ -1,12 +1,13 @@
 #' Creating a single data frame from a folder of similarly structured Excel spreadsheets
 #'
-#' Like Voltron-ing your Excel data
+#' Like Voltron-ing your Excel data. This package is based on readxl, so passing on variables from read_excel will work in bulk_excel. These variables will apply to the import of each Excel spreadsheet.
+
 #' @param folder The folder in relation to your working directory where the Excel files exist. Default folder is the current working directory.
 #' @param export File name to export csv file as, if wanted.
 #' @keywords data appending
 #' @export
 #' @examples
-#' bulk_csv(folder="data", export="combined_data.csv")
+#' bulk_excel(folder="data", export="combined_data.xls")
 
 # This is the read.csv method using lapply
 
@@ -28,9 +29,17 @@
 
 
 
-bulk_excel <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv"){
+bulk_excel <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkcsv2017.csv",  sheet = 1, col_names = TRUE, col_types = NULL, na = "",
+                       skip = 0){
   require(readxl)
   require(dplyr)
+  require(readr)
+
+  sheet_num_f <- sheet
+  col_names_f <- col_names
+  col_types_f <- col_types
+  na_f <- na
+  skip_f <- skip
 
   if (folder=="DEFAULTBULKCSV2017" | folder=="") {
     folder <- getwd()
@@ -46,7 +55,8 @@ bulk_excel <- function(folder="DEFAULTBULKCSV2017", export="filenamedefaultbulkc
   pb <- txtProgressBar(min = 0, max = length(files), style = 3)
 
   for (i in 1:length(files)) {
-    file_x <- read_excel(paste0(folder, "/", files[i]))
+    file_x <- read_excel(paste0(folder, "/", files[i]),  sheet = sheet_f, col_names = col_names_f , col_types = col_types_f, na = na_f,
+                         skip = skip_f)
     colnames(file_x) <- ifelse(is.na(colnames(file_x)), "", colnames(file_x))
     if (i ==1) {
       all_files <- file_x
